373c9aaec52fd9fdcb78d1b2d68532f7137d4fbe
Manuel <manuel.kleinert@gmail.com>
2014-05-30 15:53:56 +0200

    First Commit

---
+++ xGallery.js
@@ -0,0 +1,215 @@
+/*#####################################################################################################################
+                                                                                                              
+                    PPPPPPPPPPPPPPPPP                                                                              
+                    P::::::::::::::::P                                                                             
+                    P::::::PPPPPP:::::P                                                                            
+                    PP:::::P     P:::::P                                                                           
+xxxxxxx      xxxxxxx  P::::P     P:::::Paaaaaaaaaaaaa     ggggggggg   ggggg    eeeeeeeeeeee    rrrrr   rrrrrrrrr   
+ x:::::x    x:::::x   P::::P     P:::::Pa::::::::::::a   g:::::::::ggg::::g  ee::::::::::::ee  r::::rrr:::::::::r  
+  x:::::x  x:::::x    P::::PPPPPP:::::P aaaaaaaaa:::::a g:::::::::::::::::g e::::::eeeee:::::eer:::::::::::::::::r 
+   x:::::xx:::::x     P:::::::::::::PP           a::::ag::::::ggggg::::::gge::::::e     e:::::err::::::rrrrr::::::r
+    x::::::::::x      P::::PPPPPPPPP      aaaaaaa:::::ag:::::g     g:::::g e:::::::eeeee::::::e r:::::r     r:::::r
+     x::::::::x       P::::P            aa::::::::::::ag:::::g     g:::::g e:::::::::::::::::e  r:::::r     rrrrrrr
+     x::::::::x       P::::P           a::::aaaa::::::ag:::::g     g:::::g e::::::eeeeeeeeeee   r:::::r            
+    x::::::::::x      P::::P          a::::a    a:::::ag::::::g    g:::::g e:::::::e            r:::::r            
+   x:::::xx:::::x   PP::::::PP        a::::a    a:::::ag:::::::ggggg:::::g e::::::::e           r:::::r            
+  x:::::x  x:::::x  P::::::::P        a:::::aaaa::::::a g::::::::::::::::g  e::::::::eeeeeeee   r:::::r            
+ x:::::x    x:::::x P::::::::P         a::::::::::aa:::a gg::::::::::::::g   ee:::::::::::::e   r:::::r            
+xxxxxxx      xxxxxxxPPPPPPPPPP          aaaaaaaaaa  aaaa   gggggggg::::::g     eeeeeeeeeeeeee   rrrrrrr            
+                                                                   g:::::g                                         
+                                                       gggggg      g:::::g                                         
+                                                       g:::::gg   gg:::::g                                         
+                                                        g::::::ggg:::::::g                                         
+                                                         gg:::::::::::::g                                          
+                                                           ggg::::::ggg                                            
+                                                              gggggg
+															  
+© xPager - xGallery - Manuel Kleinert - www.xpager.ch - info(at)xpager.ch - v 0.0.1 - 28.05.2014
+#####################################################################################################################*/
+
+(function($){
+	$.fn.xGallery = function(options){
+		if(!options){var options = {};}
+		return this.each(function() {
+			options.obj = this;
+			new xGallery(options);
+		});
+	}
+}(jQuery));
+
+var xGallery = function(options,fx){
+	this.options = $.extend({
+        id:false,
+        obj:false,
+        border:200,
+        beta:true
+    },options);
+
+	// Options to Attributs
+	for(var name in this.options){eval("this."+name+"=this.options."+name);}
+
+    // Attreibute
+    this.count = 0;
+    this.activImage = 0;
+    this.images = new Array();
+    this.imagesThumb = new Array();
+    this.width = $(window).width();
+    this.height = $(window).height();
+    
+    this.init();
+}
+
+xGallery.prototype = {
+    init:function(){
+        var self = this;
+       
+        // Set Obj
+        if(this.options.obj){
+            this.obj =  this.options.obj;	
+        }else{
+            this.obj =  $(this.options.id);	
+        }
+        
+        this.imagesThumb = $(this.obj).find("img");
+        this.imageNum = $(this.imagesThumb).length;
+        
+        // Set Images (a Tag Href load)
+        $(this.imagesThumb).each(function(i,obj) {
+            var img = new Image();
+            img.src = $(obj).attr("data-img");
+            self.images.push(img);
+        });
+        
+        this.loadGallery();
+    },
+    
+    loadGallery:function(){
+        var self = this;
+        var num = 0;
+        $(this.imagesThumb).each(function(i,obj){
+            self.imageLoader(obj,function(){
+				num++;
+				if(num==self.imageNum){self.build();}
+			},function(){
+				self.message("Error load image "+(i+1));
+				$(obj).remove();
+				self.imagesThumb--;
+                self.images--;
+				if(num==self.imageNum){self.build();}
+			});
+        });
+    }, 
+    
+    build:function(){
+        var self = this;
+        
+        var html = "<div class='inner-content'>";
+        $(this.images).each(function(i,obj) {
+            if(obj){
+                var setClass = "";				
+                if(self.scale){
+                    if(obj["image"].width < obj["image"].height && self.verticalFormat){
+                        setClass += " verticalFormat";
+                    }else{
+                        setClass += " horizontalFormat";
+                    }
+                }else{
+                    setClass += " noScaleFormat";
+                }
+                if(self.detectBrightness){
+                    setClass += " "+obj["brightness"];
+                }
+                if(self.shadowBox){setClass += " shadowBox";}
+                html += "<div class='image-content'><div class='image "+setClass+"' style='";
+                html += "width:"+(self.width-self.border)+"px;";
+                html += "height:"+(self.height-self.border)+"px;";
+                html += "background-image:url("+obj.src+");";
+                html += "'>&nbsp;</div>";
+                if(self.showComments && typeof obj["comment"] != 'undefined' && obj["comment"] != ''){
+                    html += "<div class='comments'><div class='comments-content'>"+obj["comment"]+"</div>";
+                }
+                html += "</div>";
+            }
+        });
+		html += "</div></div>";
+        
+        $(this.obj).append("<div class='surface'></div>");
+        $(this.obj).append("<div class='overflow' style='width:"+(self.width-self.border)+"px;height:"+(self.height-self.border)+"px;'>"+html+"</div>");
+
+        this.startGallery();
+    },
+    
+    startGallery:function(){
+        var self = this;
+        
+        $(window).resize(function(){
+            self.setSize();    
+        });
+        
+        $(this.imagesThumb).click(function(){
+            self.activImage = $(this).parent(".xpager-conainer").index();
+            self.openGallery();
+        });
+        
+        $(this.obj).find(".surface").click(function(){
+            self.closeGallery(); 
+        });
+        
+        $(this.imagesThumb).fadeIn(500);
+    },
+    
+    openGallery:function(){
+        var self = this;
+        $(this.obj).find(".image-content").hide();
+        $(this.obj).find(".image-content").eq(this.activImage).css("display","block");
+        $(this.obj).find(".surface").fadeIn(500,function(){
+            $(self.obj).find(".overflow").fadeIn(500); 
+        });
+       
+    },
+    
+    closeGallery:function(){
+        var self = this;
+        $(this.obj).find(".overflow").fadeIn(500,function(){
+            $(self.obj).find(".surface").fadeIn(500); 
+        });  
+    },
+    
+    imageLoader:function(img,fx,fxErr){
+		if(img.complete||img.readyState===4){
+			img.src+="?d="+new Date().getTime();
+			$(img).load(function(response,status,xhr){
+                if(fx){fx();}}).error(function(){if(fxErr){fxErr();}
+            });
+		}else{
+			if(fx){fx();}
+		}
+	},
+    
+    setSize:function(){
+		var self = this;
+		var innerContent = $(this.obj).find(".overflow .inner-content");
+		this.width = $(window).width();
+        this.height = $(window).height();
+        $(this.obj).find(".overflow .image-content .image, .overflow").css({"width":this.width-this.border,"height":this.height-this.border});
+	},
+    
+    // Console
+	message:function(txt){
+		if(this.beta){
+			if($("#cis_error_message").length){
+				var html = "<div class='cis_error_message' style='font-size:10px; text-align:left; line-height: 25px; border-bottom:solid 1px #ddd; padding-left:5px;'><div>";
+				$("#cis_error_message_titel").after(html);
+				$(".cis_error_message").first().text("- "+txt);
+			}else{
+				var html = "<div id='cis_error_message' style='position:absolute; top:10px; left:10px; z-index:100000; height:100px; width:250px; border:solid 2px #000; display:none;background-color:#fff; overflow: auto;'>";
+				html += "<h3 id='cis_error_message_titel' style='display:block; text-align:center; background-color:#666; color:#fff; font-size:12px; line-height: 25px!important; margin:0px;'>Console</h3>";
+				html += "<div class='cis_error_message' style='font-size:10px; text-align:left; line-height: 25px; border-bottom:solid 1px #ddd; padding-left:5px;'><div>";
+				html += "</div>";			
+				$(this.obj).append(html);
+				$(".cis_error_message").text("- "+txt);
+				$("#cis_error_message").fadeIn(500);
+			}
+		}
+	}
+}
